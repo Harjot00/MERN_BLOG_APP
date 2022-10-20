b7b94d7d48ef7cf545204b503c77fd646d9d42a5
Harjot Singh <hsbinsardari12g@outlook.com>
2022-10-20 08:33:20 -0400

    updates

--- backend/start.js
+++ backend/start.js
@@ -24,6 +24,6 @@ app.use(bodyParser.json());
 app.use("/", userRouter);
 app.use("/", blogRouter);
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server started");
 });
