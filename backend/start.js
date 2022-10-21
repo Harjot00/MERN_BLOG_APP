84c4a11fc6ba49eda6a4bbf13c521379a0bfbae4
Harjot Singh <hsbinsardari12g@outlook.com>
2022-10-21 04:27:55 -0400

    path fixed

--- backend/start.js
+++ backend/start.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
-const root = require("path").resolve("../");
+const appRoot = require("app-root-path");
 const app = express();
 const userRouter = require("./routes/userRoutes");
 const blogRouter = require("./routes/blogRoutes");
@@ -23,19 +23,16 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(root, "frontend", "build")));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
 }
 
 app.use("/api", userRouter);
 app.use("/api", blogRouter);
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(root, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..frontend", "build"));
 });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started");
 });
-
-const heroku = path.join(__dirname);
-console.log(heroku);
