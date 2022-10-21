a425aa1ec54af3f3440766ea38c84bd22079b342
Harjot Singh <hsbinsardari12g@outlook.com>
2022-10-21 02:24:52 -0400

    api routes renamed

--- backend/start.js
+++ backend/start.js
@@ -22,11 +22,15 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("./frontend/build"));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
 }
 
 app.use("/api", userRouter);
 app.use("/api", blogRouter);
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started");
